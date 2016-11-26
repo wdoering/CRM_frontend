bddf0230a8ff522d3468eed3ab5905844a1d7885
Wagner <wdoering@gmail.com>
2016-11-26 12:38:55 -0200

    code indentation.

--- www/components/product/product-service.js
+++ www/components/product/product-service.js
@@ -1,51 +1,51 @@
-angular.module('CRM').service('ProductService', function($http, SERVER_URL, MANUFACTURER_CTRL, PRODUCT_CTRL, PRODUCT_TYPE_CTRL, PRODUCT_SIZE_CATEGORY_CTRL){
-  var urlProduct = "/product";
-  return {
-      all: function(){
-        return [{id:1, description:'product 1 STATIC'},{id:2, description:'product 2 STATIC'},{id:3, description:'product 3 STATIC'}];
-      },
-      
-      getProducts: function(){
-          console.log(SERVER_URL);
-        return $http.get(SERVER_URL + PRODUCT_CTRL);
-      },
+angular.module('CRM').service('ProductService', function($http, SERVER_URL, MANUFACTURER_CTRL, PRODUCT_CTRL, PRODUCT_TYPE_CTRL, PRODUCT_SIZE_CATEGORY_CTRL) {
+    var urlProduct = "/product";
+    return {
+        all: function() {
+            return [{ id: 1, description: 'product 1 STATIC' }, { id: 2, description: 'product 2 STATIC' }, { id: 3, description: 'product 3 STATIC' }];
+        },
 
-      submitNewProduct : function(productData){
-        return $http.post(SERVER_URL + PRODUCT_CTRL, productData);
-      },
+        getProducts: function() {
+            console.log(SERVER_URL);
+            return $http.get(SERVER_URL + PRODUCT_CTRL);
+        },
 
-      deleteProduct : function(productId){
-        return $http.delete(SERVER_URL + PRODUCT_CTRL + "?id=" + productId); 
-      },
-      submitEditProduct : function(product){
-        return $http.put(SERVER_URL + PRODUCT_CTRL, product);
-      },
-       
-      //this function should be moved to a generic class file...
-      fetchIndex : function (objList,productId) {
-        var indexofObject = -1;
-        angular.forEach(objList, function(product) {
-             
-             if(productId === product.id){
-                console.log(objList.indexOf(product));
-                indexofObject = objList.indexOf(product);
-             }
-             
-        });
-        return indexofObject;
-      },
-      
-      getManufacturers: function(){
-        return $http.get(SERVER_URL + MANUFACTURER_CTRL);
-      },
-      getProductTypes: function(){
-        return $http.get(SERVER_URL + PRODUCT_TYPE_CTRL);
-      },
-      getProductSizeCategories: function(){
-        return $http.get(SERVER_URL + PRODUCT_SIZE_CATEGORY_CTRL);
-      }
+        submitNewProduct: function(productData) {
+            return $http.post(SERVER_URL + PRODUCT_CTRL, productData);
+        },
+
+        deleteProduct: function(productId) {
+            return $http.delete(SERVER_URL + PRODUCT_CTRL + "?id=" + productId);
+        },
+        submitEditProduct: function(product) {
+            return $http.put(SERVER_URL + PRODUCT_CTRL, product);
+        },
+
+        //this function should be moved to a generic class file...
+        fetchIndex: function(objList, productId) {
+            var indexofObject = -1;
+            angular.forEach(objList, function(product) {
+
+                if (productId === product.id) {
+                    console.log(objList.indexOf(product));
+                    indexofObject = objList.indexOf(product);
+                }
+
+            });
+            return indexofObject;
+        },
+
+        getManufacturers: function() {
+            return $http.get(SERVER_URL + MANUFACTURER_CTRL);
+        },
+        getProductTypes: function() {
+            return $http.get(SERVER_URL + PRODUCT_TYPE_CTRL);
+        },
+        getProductSizeCategories: function() {
+            return $http.get(SERVER_URL + PRODUCT_SIZE_CATEGORY_CTRL);
+        }
 
 
-  }
+    }
 
 })
